6438a35cec71a041ab272fc872c0de2af60ff1d0
Gerardohb99 <gerardohb99@gmail.com>
2020-12-08 10:54:20 +0100

    FUNCIONA CON FSTREAM

--- main.cpp
+++ main.cpp
@@ -27,17 +27,17 @@ struct BMP
 
 bool readBMP(string *filename, BMP* bmp)
 {
-    FILE *f = fopen(filename->c_str(), "rb");
+    ifstream f (filename->c_str(), ios::in | ios::binary);
 
     // read the first part of the header
     bmp->header.resize(14);
-    fread(&bmp->header[0], sizeof(byte), 14, f);
+    f.read((char*)&bmp->header[0], 14);
     int start = *(int *)&bmp->header[10];
 
     // read the remaining part of the header
-    fseek(f, 0, SEEK_SET);
+    f.seekg(0);
     bmp->header.resize(start);
-    fread(&bmp->header[0], sizeof(byte), start, f);
+    f.read((char*)&bmp->header[0], start);
 
     // Comprobacion de erroresen el header
     byte dims = *(byte *)&bmp->header[27] << 8 | *(byte *)&bmp->header[26];
@@ -76,17 +76,17 @@ bool readBMP(string *filename, BMP* bmp)
         for (int j = 0; j < width; j++){
 
             Color color {};
-            fread(&color.B, sizeof(byte), 1, f);
-            fread(&color.G, sizeof(byte), 1, f);
-            fread(&color.R, sizeof(byte), 1, f);
+            f.read((char*)&color.B, 1);
+            f.read((char*)&color.G, 1);
+            f.read((char*)&color.R, 1);
 
             bmp->data[i][j] = color;
         }
         // skipping padding
-        fseek(f, padding, SEEK_CUR);
+        f.seekg((int)f.tellg() + padding);
     }
 
-    fclose(f);
+    f.close();
     return true;
 }
 
@@ -98,7 +98,8 @@ void writeInByteArray(byte* dest, byte info[], int infoSize){
 
 void writeBMP(string *filename, BMP *bmp)
 {
-    FILE *f = fopen(filename->c_str(), "wb");
+    //FILE *f = fopen(filename->c_str(), "wb");
+    ofstream f (filename->c_str(), ios::out | ios::binary);
     int width = bmp->data[0].size();
     int height = bmp->data.size();
     int padding = (4 - (3 * width % 4)) % 4;
@@ -164,21 +165,21 @@ void writeBMP(string *filename, BMP *bmp)
 
     writeInByteArray(&bmp->header[50], zeroArray, sizeof(int));
 
-    fwrite(&bmp->header[0], sizeof(byte), 54, f);
+    f.write((char*)&bmp->header[0], 54);
 
     byte zeros[8] = { };
 
     for (int i = 0; i < height; i++) {
         for(int j = 0; j < width; j++) {
             Color pixel = bmp->data[i][j];
-            fwrite(&pixel.B, sizeof(byte), 1, f);
-            fwrite(&pixel.G, sizeof(byte), 1, f);
-            fwrite(&pixel.R, sizeof(byte), 1, f);
+            f.write((char*)&pixel.B, 1);
+            f.write((char*)&pixel.G, 1);
+            f.write((char*)&pixel.R, 1);
         }
-        fwrite(&zeros[0], sizeof(byte), padding, f);
+        f.write((char*)&zeros[0], padding);
     }
 
-    fclose(f);
+    f.close();
 }
 
 bool printError(int argc, string *command, string *indir, string *outdir)
